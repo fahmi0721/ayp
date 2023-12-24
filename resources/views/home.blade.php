d11b7b6affc44bd885182010693a6d2fbb6e96be
fahmi0721 <fahmiidrus131@gmail.com>
2023-12-25 02:03:48 +0800

    new update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -28,8 +28,19 @@
 		    var tokenCSRF   = jQuery('meta[name="csrf-token"]').attr('content');
             var url_link    = "{{ asset('/') }}";
     </script>
+  <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-TVC2PGKJ');</script>
+    <!-- End Google Tag Manager -->
 </head>
 <body class="hold-transition sidebar-mini text-sm">
+  <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TVC2PGKJ"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 <!-- Site wrapper -->
 <div class="wrapper">
   <!-- Navbar -->
